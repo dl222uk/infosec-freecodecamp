f6c3537a50274227e06d4f2295f0fab80a84de21
David Le <davidbooab@hotmail.com>
2024-10-10 12:56:39 +0200

    First commit

--- myApp.js
+++ myApp.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 
+const express = require("express");
 
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
 
 
